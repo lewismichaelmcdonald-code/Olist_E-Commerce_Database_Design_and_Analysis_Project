895b6f042e4b353fbe6bd7a799ff44b96091cd3c
LewisM-Code <lewismichaelmcdonald@gmail.com>
2026-05-29 00:10:23 +0100

    Updated LTV query

--- Olist_E-Commerce_Analysis_Project/2_Customer_Lifetime_Value.sql
+++ Olist_E-Commerce_Analysis_Project/2_Customer_Lifetime_Value.sql
@@ -1,3 +1,4 @@
+--CTE 1, Finding cohort month
 WITH cohort_months AS (
     SELECT 
         customer_info.customer_unique_id,
@@ -10,7 +11,24 @@ WITH cohort_months AS (
         customer_info.customer_unique_id
 ),
 
-WITH order_revenue AS (
+--CTE 2, using cohort month to calculate a cohort index
+index AS (
+    SELECT
+        orders.order_id,
+        cm.cohort_month,
+        DATE_TRUNC('month',orders.order_purchase_timestamp) AS order_month,
+        ((EXTRACT(YEAR FROM(DATE_TRUNC('month',orders.order_purchase_timestamp))) - EXTRACT(YEAR FROM cm.cohort_month)) * 12) +
+        (EXTRACT(MONTH FROM(DATE_TRUNC('month',orders.order_purchase_timestamp))) - EXTRACT(MONTH FROM cm.cohort_month)) AS cohort_index
+    FROM
+        orders_fact AS orders
+    INNER JOIN customer_info_dim AS customer_info
+        ON orders.customer_id = customer_info.customer_id
+    INNER JOIN cohort_months AS cm
+        ON customer_info.customer_unique_id = cm.customer_unique_id
+),
+
+--CTE 3, calculating total revenue or each order
+order_revenue AS (
     SELECT
     order_payments_fact.order_id,
     SUM(order_payments_fact.payment_value) AS total_order_revenue
@@ -18,4 +36,35 @@ WITH order_revenue AS (
         order_payments_fact
     GROUP BY
         order_payments_fact.order_id
+),
+
+-- CTE 4: Calculate the total starting size of each cohort
+cohort_sizes AS (
+    SELECT 
+        cohort_month,
+        COUNT(DISTINCT customer_unique_id) AS total_starting_customers
+    FROM 
+        cohort_months
+    GROUP BY 
+        cohort_month
 )
+
+-- Main query link cohort index with order revenue and cohort sizes
+SELECT
+    index.cohort_month,
+    cs.total_starting_customers,
+    index.cohort_index,
+    SUM(order_revenue.total_order_revenue) AS total_revenue
+FROM
+    index
+INNER JOIN order_revenue
+    ON index.order_id = order_revenue.order_id
+INNER JOIN cohort_sizes AS cs 
+    ON index.cohort_month = cs.cohort_month 
+GROUP BY
+    index.cohort_month,
+    cs.total_starting_customers, 
+    index.cohort_index
+ORDER BY
+    index.cohort_month ASC,
+    index.cohort_index ASC;
